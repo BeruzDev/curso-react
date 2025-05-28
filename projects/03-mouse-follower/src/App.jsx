6efef9ca7e6cfd03a08b4c68c273e3ad70d8368a
BeruzDev <albertus1291@gmail.com>
2025-05-28 18:52:18 +0200

    Follow pointer effect done!

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,10 +1,51 @@
+import { useState, useEffect } from 'react'
+import './App.css'
 
-function App() {
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('efecto', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //Cleanup
+    //-> Se ejecuta cuando se desmonta el componente
+    //-> O se ejecuta cuando cambian las dependencias antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <h3>
-      Proyecto 3
-    </h3>
+    <>
+      <div
+        className={`bolita${!enabled ? 'disabled' : ''}`}
+        style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar ' : 'Activar '}
+        seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
