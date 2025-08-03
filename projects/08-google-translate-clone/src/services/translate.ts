ead4870eb54ff1fd20ab7de7c171c90d0cf0c276
BeruzDev <albertus1291@gmail.com>
2025-08-03 17:36:18 +0200

    .env update

--- projects/08-google-translate-clone/src/services/translate.ts
+++ projects/08-google-translate-clone/src/services/translate.ts
@@ -1,3 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 //!! ESTA PARTE DEBE ESTAR EN EL BACKEND!! SINO SE COLARA LA API KEY!!!!!
+
+// TODO: 1.Hacer una api -> 2.Atacar a esa API -> 3.Api ataca a openai
+
+const apiKey = import.env.VITE_OPENAI_API_KEY
